e9db9192c70856d333fbc626f286e56ee6858e75
Hai Zhu <zhuh1106@gmail.com>
2025-01-23 10:15:50 +0100

    isdf: nh3_dimer aug-ccpvdz basis

---
+++ isdf/NH3_dimer/aug-ccpvdz/matlab_eri_nh3_dimer_aug.m
@@ -0,0 +1,88 @@
+%
+%
+% 01/20/25 Hai
+
+% profile clear
+% profile on
+
+addpath('../../../')
+addpath('../../../utils/')
+addpath('../../../treefun/')
+
+clear all
+order = 7;
+eps = 1e-02; %
+% eps = 1e-04; %
+% eps = 1e-05;
+
+% create some verification mesh, supposedly to be the same as python_eri_nh3_dimer.py
+% run python_eri_nh3_dimer.py to load x,y,z coordinates and basis from pyscf
+if 1
+  load('nh3_dimer_aug_basis_check.mat')
+  [x2, y2, z2] = ndgrid(linspace(0,1,5), linspace(0,1,5), linspace(0,1,5));
+  xyz2 = [x2(:) y2(:) z2(:)];
+  func = @(x,y,z) cgtofunc_nh3_dimer_aug_ccpvdz(x,y,z);
+  vals2 = func(x2, y2, z2);
+  vals2rs = reshape(vals2,[],100);
+  diff = abs(vals - reshape(vals2rs,[],100));
+  max(diff(:))
+end
+
+% keyboard
+
+%%% resolve tree on cgto^2
+func2 = @(x,y,z) cgto2func_nh3_dimer_aug_ccpvdz(x,y,z);
+% func2 = @(x,y,z) cgtofunc_nh3_dimer_aug_ccpvdz(x,y,z);
+checkpts = [
+          -1.578718  -0.046611   0.000000;... 
+          -2.158621   0.136396  -0.809565;...
+          -2.158621   0.136396   0.809565;...
+          -0.849471   0.658193   0.000000;...
+           1.578718   0.046611   0.000000;...
+           2.158621  -0.136396  -0.809565;...
+           0.849471  -0.658193   0.000000;...
+           2.158621  -0.136396   0.809565
+           ]';
+opts = struct('balance',true,...
+              'tol',eps, ...
+              'checkpts',checkpts, ... 
+              'ifcoeffs',false);
+f = treefun3(func2,[-15 15 -15 15 -15 15],order,opts); 
+plot(f,func2);
+
+%%% treefun to bdmk
+Norb = 100;
+ndim = 3;
+ratio = 0.5/15; % from boxlen to 1
+ipoly = 0;
+[src,nleafbox,srcleaf,wtsleaf,norder,npbox,nboxes,nlevels,ltree,itree,iptr,centers,boxsize] = treefun2bdmk(f,ndim,ratio,ipoly);
+
+%%% eval cgto
+src0 = src/ratio;
+func = @(x,y,z) cgtofunc_nh3_dimer_aug_ccpvdz(x,y,z);
+fvals0 = squeeze(func(squeeze(src0(1,:,:)),squeeze(src0(2,:,:)),squeeze(src0(3,:,:))));
+fvals0 = permute(fvals0,[3 1 2]);
+fvals = fvals0;
+
+%%% compute V_ijkl
+nd = Norb*(Norb+1)/2;
+ikernel = 1;
+beta = 6.0d0;
+Vijkl = Vijklcomp(Norb,ratio,fvals,nleafbox,srcleaf,wtsleaf,...
+                ndim,eps,ikernel,beta,ipoly,norder,npbox, ...
+                nboxes,nlevels,ltree,itree,iptr,centers,boxsize);
+
+%%% save data
+eps_str = sprintf('%.0e', eps);
+mat_filename = sprintf('ERI_nh3_dimer_aug_ccpvdz_%s.mat', eps_str);
+h5_filename = sprintf('ERI_nh3_dimer_aug_ccpvdz_%s.h5', eps_str);
+save(mat_filename,'Vijkl')    
+if exist(h5_filename, 'file') ~= 2
+  h5create(h5_filename,"/DS1",[Norb Norb Norb Norb])
+end
+h5write(h5_filename,"/DS1",Vijkl)
+h5disp(h5_filename)
+
+% profile viewer
+
+keyboard
---
+++ isdf/NH3_dimer/aug-ccpvdz/python_eri_nh3_dimer_aug.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import numpy as np
+# from pyscf import gto, ao2mo
+from pyscf import gto
+import h5py
+from scipy.io import savemat
+
+geom='''
+        N  -1.578718  -0.046611   0.000000 
+        H  -2.158621   0.136396  -0.809565
+        H  -2.158621   0.136396   0.809565
+        H  -0.849471   0.658193   0.000000
+        N   1.578718   0.046611   0.000000
+        H   2.158621  -0.136396  -0.809565
+        H   0.849471  -0.658193   0.000000
+        H   2.158621  -0.136396   0.809565
+        '''
+
+mol_nh3_dimer = gto.M(
+        verbose=7, 
+        atom=geom, 
+        basis='aug-ccpvdz')
+
+# 
+x, y, z = np.meshgrid(np.linspace(0,1,5),np.linspace(0,1,5),np.linspace(0,1,5),
+                         indexing='ij')
+xyz = np.column_stack([x.flatten(order='F'),y.flatten(order='F'),z.flatten(order='F')])
+
+# Evaluate GTOs at the specified points
+vals = np.array(mol_nh3_dimer.eval_gto('GTOval_sph',xyz))
+print(vals.shape)
+savemat("nh3_dimer_aug_basis_check.mat", {'x': x, 'y': y, 'z': z, 'xyz': xyz, 'vals': vals})
+
+# Calculate ERI for a given molecule and a AO basis: 
+# eri has dimenions (nao, nao, nao, nao) where nao is the number of AO basis functions
+eri = mol_nh3_dimer.intor('int2e')
+print(eri)
+print(eri.shape)
+
+# Read ERI from Hai
+with h5py.File('ERI_nh3_dimer_aug_ccpvdz_1e-02.h5', 'r') as ar:
+    eri0 = ar['DS1'][()]
+
+diff = eri - eri0
+print("Maximum diff between two ERIs:")
+print(np.max(np.abs(diff)))
