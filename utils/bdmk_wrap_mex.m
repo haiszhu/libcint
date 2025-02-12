3f47a68508f5919f606a5f689d525c9615ae056f
Hai Zhu <zhuh1106@gmail.com>
2025-02-12 15:47:07 -0500

    ...

--- utils/bdmk_wrap_mex.m
+++ utils/bdmk_wrap_mex.m
@@ -4,4 +4,5 @@ npboxnboxes = npbox*nboxes;
 fvals = reshape(fvals,[nd npboxnboxes]);
 mex_id_ = 'bdmk_wrap(i int[x], i int[x], i double[x], i int[x], i double[x], i int[x], i int[x], i int[x], i int[x], i int[x], i int[x], i int[x], i int[x], i double[xx], i double[x], i double[xx], io double[xx])';
 [pot] = bdmk_module(mex_id_, nd, ndim, eps, ikernel, beta, ipoly, norder, npbox, nboxes, nlevels, ltree, itree, iptr, centers, boxsize, fvals, pot, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ltree, 8, ndim, nboxes, nlevelsp1, nd, npboxnboxes, nd, npboxnboxes);
+pot = reshape(pot,[nd npbox nboxes]);
 end
