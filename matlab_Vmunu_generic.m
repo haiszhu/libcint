f04771bf3f7c03fb7b9cd2934f77c57ce5bab5ef
Hai Zhu <zhuh1106@gmail.com>
2025-02-11 22:28:20 -0500

    Vmunu wrap

---
+++ matlab_Vmunu_generic.m
@@ -0,0 +1,114 @@
+%
+%
+% 02/11/25 Hai
+
+profile clear
+profile on
+
+addpath('./utils/')
+addpath('./treefun/')
+addpath('./data')
+
+clear all
+order = 8;
+eps = 1e-06; 
+
+%%% resolve tree on cgto^2
+rad = 15;
+geom = sprintf([ ...
+    'O    0    0.       0.\n',...
+    'H    0    -0.757   0.587\n',...
+    'H    0    0.757    0.587\n']),
+basmod = 'cc-pvdz.dat';
+basis = fullfile(fileparts(mfilename('fullpath')), './basis', basmod);
+mol = gto(geom,basis);
+eval_name = 'GTOval_sph';
+opts = struct('balance',true,...
+              'tol',eps, ...
+              'checkpts',mol.checkpts, ... 
+              'ifcoeffs',false);
+func2 = @(x,y,z) mol.eval_gto2(eval_name, cat(4,x,y,z));
+f = treefun3(func2,[-rad rad -rad rad -rad rad],order,opts);
+plot(f,func2);
+
+%%% treefun to bdmk
+Norb = 24; % 
+ndim = 3;
+ratio = 0.5/15; % from boxlen to 1
+ipoly = 0;
+[src,nleafbox,srcleaf,wtsleaf,norder,npbox,nboxes,nlevels,ltree,itree,iptr,centers,boxsize] = treefun2bdmk(f,ndim,ratio,ipoly);
+
+r = h5read('src_h2o_ccpvdz.h5', '/DS1');
+npts = numel(r(1,:));
+r2 = reshape(r,3,[]);
+diff = abs(reshape(src/ratio,3,[]) - r2);
+
+%%% eval cgto
+src0 = src/ratio;
+func = @(x,y,z) mol.eval_gto(eval_name, cat(4,x,y,z));
+fvals0 = squeeze(func(squeeze(src0(1,:,:)),squeeze(src0(2,:,:)),squeeze(src0(3,:,:))));
+fvals0 = permute(fvals0,[3 1 2]);
+fvals = fvals0;
+
+%%% compute V_ijkl
+nd = Norb*(Norb+1)/2;
+ikernel = 1;
+beta = 6.0d0;
+% Vijkl = Vijklcomp(Norb,ratio,fvals,nleafbox,srcleaf,wtsleaf,...
+%                 ndim,eps,ikernel,beta,ipoly,norder,npbox, ...
+%                 nboxes,nlevels,ltree,itree,iptr,centers,boxsize);
+
+%%% load isdf data
+%
+idfname = 'isdf_1e-9.h5'; % 0.0026
+% idfname = 'isdf_1e-4.h5'; % 0.4476
+info = h5info(idfname);
+Np = h5read(idfname, '/Np');
+collocation_matrix = h5read(idfname, '/collocation_matrix');
+interpolating_points = h5read(idfname, '/interpolating_points');
+interpolating_vectors = h5read(idfname, '/interpolating_vectors');
+kpts = h5read(idfname, '/kpts');
+nkpts_ibz = h5read(idfname, '/nkpts_ibz');
+nqpts_ibz = h5read(idfname, '/nqpts_ibz');
+qpts = h5read(idfname, '/qpts');
+collocation_matrix = squeeze(collocation_matrix(1,:,:));
+interpolating_vectors = squeeze(interpolating_vectors(1,:,:));
+
+%%% compute
+nd = size(interpolating_vectors,2);
+Vmunu = Vmunucomp(Norb,ratio,interpolating_vectors,nleafbox,srcleaf,wtsleaf,...
+                ndim,eps,ikernel,beta,ipoly,norder,npbox, ...
+                nboxes,nlevels,ltree,itree,iptr,centers,boxsize);
+
+%%%
+Vijkl = zeros(Norb,Norb,Norb,Norb);
+for i = 1:Norb
+  for j = 1:Norb
+    collocation_matrix_ij = collocation_matrix(i,:).*collocation_matrix(j,:);
+    for k = 1:Norb
+      for l = 1:Norb
+        collocation_matrix_kl = collocation_matrix(k,:).*collocation_matrix(l,:);
+        Vijkl(i,j,k,l) = sum(Vmunu.*( collocation_matrix_ij(:) ...
+                                     .*collocation_matrix_kl(:)'),'all');
+      end
+    end
+  end
+end
+
+save('Vmunu_h2o_ccpvdz.mat','Vmunu')
+if exist("Vmunu_h2o_ccpvdz.h5", 'file') ~= 2
+  h5create("Vmunu_h2o_ccpvdz.h5","/DS1",[nd nd])
+  h5write("Vmunu_h2o_ccpvdz.h5","/DS1",Vmunu)
+  h5disp("Vmunu_h2o_ccpvdz.h5")
+end
+
+save('ERI_h2o_ccpvdz.mat','Vijkl')    
+if exist("ERI_h2o_ccpvdz.h5", 'file') ~= 2
+  h5create("ERI_h2o_ccpvdz.h5","/DS1",[Norb Norb Norb Norb])
+  h5write("ERI_h2o_ccpvdz.h5","/DS1",Vijkl)
+  h5disp("ERI_h2o_ccpvdz.h5")
+end
+
+profile viewer
+
+keyboard
---
+++ utils/Vmunucomp.m
@@ -0,0 +1,108 @@
+function Vmunu = Vmunucomp(Norb,ratio,fvals,nleafbox,srcleaf,wtsleaf,...
+                            ndim,eps,ikernel,beta,ipoly,norder,npbox, ...
+                            nboxes,nlevels,ltree,itree,iptr,centers,boxsize)
+
+%
+nhess = ndim*(ndim+1)/2;
+%%% based on fvals for all cGTOs, compute phi_k(r')*phi_l(r'), and singular volume integral
+nd = size(fvals,2);
+phi_kl = zeros(nd,npbox,nboxes); 
+for k = 1:nd
+  phi_kl(k,:,:) = reshape(fvals(:,k),[npbox nboxes]);
+end
+pot = zeros(nd,npbox,nboxes);
+nd0 = 10;
+nchnk = floor(nd/nd0);
+disp("=========Start BDMK=======");
+disp("nchnk is : " + nchnk );
+% for jchnk = 1:1
+for jchnk = 1:nchnk
+  jidxv = (jchnk-1)*nd0 + (1:nd0);
+  phi_kl0 = phi_kl(jidxv,:,:);
+  pot0=zeros(nd0,npbox,nboxes);
+  grad0=zeros(nd0,ndim,npbox,nboxes);
+  hess0=zeros(nd0,nhess,npbox,nboxes);
+  tic
+  ifpgh=1;
+  ifpghtarg=0;
+  ntarg = 100;
+  targs=zeros(ndim,ntarg); pote=zeros(nd0,ntarg);
+  grade=zeros(nd0,ndim,ntarg); hesse=zeros(nd0,nhess,ntarg);
+  timeinfo = zeros(20,1);
+  [pot0,grad0,hess0,pote,grade,hesse] = ...
+    bdmk_mex(nd0,ndim,eps,ikernel,beta,ipoly,norder,npbox, ...
+           nboxes,nlevels,ltree,itree,iptr,centers,boxsize,phi_kl0, ...
+           ifpgh,pot0,grad0,hess0,ntarg,targs, ...
+           ifpghtarg,pote,grade,hesse,timeinfo);
+  pot(jidxv,:,:) = pot0;
+  disp("jchnk is : " + jchnk + " ( total nchnk is : " + nchnk + " )");
+end
+jchnk = nchnk+1;
+jidxv = ((jchnk-1)*nd0+1):nd;
+nd0 = numel(jidxv);
+phi_kl0 = phi_kl(jidxv,:,:);
+pot0=zeros(nd0,npbox,nboxes);
+grad0=zeros(nd0,ndim,npbox,nboxes);
+hess0=zeros(nd0,nhess,npbox,nboxes);
+ifpgh=1;
+ifpghtarg=0;
+ntarg = 100;
+targs=zeros(ndim,ntarg); pote=zeros(nd0,ntarg);
+grade=zeros(nd0,ndim,ntarg); hesse=zeros(nd0,nhess,ntarg);
+timeinfo = zeros(20,1);
+[pot0,grad0,hess0,pote,grade,hesse] = ...
+bdmk_mex(nd0,ndim,eps,ikernel,beta,ipoly,norder,npbox, ...
+       nboxes,nlevels,ltree,itree,iptr,centers,boxsize,phi_kl0, ...
+       ifpgh,pot0,grad0,hess0,ntarg,targs, ...
+       ifpghtarg,pote,grade,hesse,timeinfo);
+pot(jidxv,:,:) = pot0;
+disp("=========End BDMK=======");
+
+%
+phi_ij_leaf = zeros(nd,npbox,nleafbox); % leaf box information
+potleaf = zeros(nd,npbox,nleafbox);
+jbox = 0;
+for ilev = 0:nlevels
+  bs = boxsize(ilev+1); % Adjusted for MATLAB indexing (1-based)
+  ifirstbox = itree(2 * ilev + 1);
+  ilastbox = itree(2 * ilev + 2);
+  nbloc = ilastbox - ifirstbox + 1;
+  for i = 1:nbloc
+    ibox = ifirstbox + i - 1;
+    if itree(iptr(4) + ibox - 1) == 0
+      jbox = jbox + 1;
+      for ell = 1:npbox
+        for j = 1:nd
+          phi_ij_leaf(j, ell, jbox) = phi_kl(j, ell, ibox);
+          potleaf(j, ell, jbox) = pot(j, ell, ibox);
+        end
+      end
+    end
+  end
+end
+
+%% compute smooth volume integral
+disp("=========Start Smooth Integral=======");
+% nd = nd0;
+potleaf = reshape(potleaf,nd,[])/ratio^2; 
+phi_ij_leaf = reshape(phi_ij_leaf,nd,[]);
+wtsleaf = wtsleaf(:)';
+phi_ij_leaf = phi_ij_leaf.*wtsleaf;
+disp("nd is : " + nd );
+Vmunu = phi_ij_leaf*potleaf';
+Vmunu = Vmunu/ratio^3;
+if 0 % old version pre 2025
+  Vmunu = zeros(nd,nd);
+  % ell = 1;
+  for ell = 1:nd
+    % (2*L)^2 * potleaf = (2*L)^2 * \int_{-1/2}^{1/2} (phi_k*phi_l)/|r' - r| dV
+    potleaf_ell = potleaf(ell,:,:)/ratio^2; 
+    for j = 1:nd
+      % (2*L)^3 * \int_{-1/2}^{1/2} phi_{ij} * pot_{kl} dV
+      Vmunu(j,ell) = sum(squeeze(phi_ij_leaf(j,:,:).*potleaf_ell).*wtsleaf(:,:),'all')/ratio^3;
+    end
+  end
+end
+disp("=========End Smooth Integral=======");
+
+end
