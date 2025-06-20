9ee0367f9ccf3528ef3841aa5a813acc54a18e16
Hai Zhu <zhuh1106@gmail.com>
2025-06-19 22:26:51 -0400

    test omp_num_threads

--- utils/computeVijkl_mex.m
+++ utils/computeVijkl_mex.m
@@ -2,7 +2,7 @@ function Vijkl = computeVijkl_mex(nd, Norb, idcoefs, Vmunu, Vijkl)
 Norbtp1d2 = Norb*(Norb+1)/2;
 Norb2 = Norb^2;
 Vijkl = reshape(Vijkl,[Norb2 Norb2]);
-mex_id_ = 'computeVijkl(i int[x], i int[x], i double[xx], i double[xx], io double[xx])';
+mex_id_ = 'computeVijkl(c i int[x], c i int[x], c i double[xx], c i double[xx], c io double[xx])';
 [Vijkl] = helper_module(mex_id_, nd, Norb, idcoefs, Vmunu, Vijkl, 1, 1, nd, Norbtp1d2, nd, nd, Norb2, Norb2);
 Vijkl = reshape(Vijkl,[Norb Norb Norb Norb]);
 end
